6290c7db0bec5d6f6b8f930a7d04971f32e4bac2
Nilesh Pharate <nileshpharate94@gmail.com>
2016-12-19 15:53:05 -0600

    final_code_commit

---
+++ Alexa_Speech_Recogni/speech_recog/src/Alexa.java
@@ -0,0 +1,42 @@
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+public class Alexa {
+
+    public static void main(String[] args) {
+        ConfigurationManager cm;
+
+        if (args.length > 0) {
+            cm = new ConfigurationManager(args[0]);
+        } else {
+            cm = new ConfigurationManager(Alexa.class.getResource("Alexa.config.xml"));
+        }
+
+        Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
+        recognizer.allocate();
+
+        Microphone microphone = (Microphone) cm.lookup("microphone");
+        if (!microphone.startRecording()) {
+            System.out.println("Cannot start microphone.");
+            recognizer.deallocate();
+            System.exit(1);
+        }
+
+        System.out.println("Say: (deploy) (my) (code) (to) (dev)(one)");
+
+        while (true) {
+            
+            Result result = recognizer.recognize();
+
+            if (result != null) {
+                String resultText = result.getBestFinalResultNoFiller();
+                System.out.println("You said: " + resultText + "\n");
+            } else {
+                System.out.println("I can't hear what you said.\n");
+            }
+        }
+    }
+}
